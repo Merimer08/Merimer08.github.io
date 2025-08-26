5e5200a379211d4685d863c0ba71673d935806f3
Your Name <you@example.com>
2025-08-26 11:37:32 +0200

    cards

--- js/layout.js
+++ js/layout.js
@@ -1,15 +1,21 @@
 // js/layout.js
 class SiteHeader extends HTMLElement {
   async connectedCallback() {
-    const res = await fetch('/partials/header.html'); 
+    const res = await fetch('partials/header.html'); // usa ruta relativa, no con "/"
     this.innerHTML = await res.text();
+    // Después de insertar el header, relanza el traductor
+    if (typeof initTranslations === 'function') {
+      initTranslations();
+    }
   }
 }
+
 class SiteFooter extends HTMLElement {
   async connectedCallback() {
-    const res = await fetch('/partials/footer.html');
+    const res = await fetch('partials/footer.html');
     this.innerHTML = await res.text();
   }
 }
+
 customElements.define('site-header', SiteHeader);
 customElements.define('site-footer', SiteFooter);
